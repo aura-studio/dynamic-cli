255ca9667ff6f4131ccfa4ed530fd9565c872c5d
lizongti <lzongti@gmail.com>
2022-12-29 19:00:48 +0800

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/aura-studio/dynamic-cli/master/Dockerfile -O /tmp/dynamic-cli.Dockerfile
-docker build -t dynamic-cli:latest -f /tmp/dynamic-cli.Dockerfile --build-arg VERSION=latest .
+docker build --no-cache -t dynamic-cli:latest -f /tmp/dynamic-cli.Dockerfile --build-arg VERSION=latest .
