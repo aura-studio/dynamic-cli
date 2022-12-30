a96bf0f24b71451918da649ce47e6e842aec03ac
lizongti <lzongti@gmail.com>
2022-12-30 13:38:04 +0800

    update builder.sh

--- builder/template_builder_sh.go
+++ builder/template_builder_sh.go
@@ -9,6 +9,6 @@ export CGO_ENABLED=1
 export GO111MODULE=on
 export GOPRIVATE={{.Module}}
 go mod tidy
-go build -o {{.House}}/{{.Name}}_{{.Version}}/libcgo_{{.Name}}_{{.Version}}.so -buildvcs=false -buildmode=c-shared -ldflags="{{.LDFlags}}" {{.House}}/{{.Name}}_{{.Version}}/libcgo_{{.Name}}_{{.Version}} #&& upx -1 {{.House}}/{{.Name}}_{{.Version}}/libcgo_{{.Name}}_{{.Version}}.so
-go build -o {{.House}}/{{.Name}}_{{.Version}}/libgo_{{.Name}}_{{.Version}}.so -buildvcs=false -buildmode=plugin -ldflags="{{.LDFlags}}-r {{.House}}/{{.Name}}_{{.Version}}/" {{.House}}/{{.Name}}_{{.Version}}/libgo_{{.Name}}_{{.Version}} #&& upx -1 {{.House}}/{{.Name}}_{{.Version}}/libgo_{{.Name}}_{{.Version}}.so
+go build -o {{.House}}/{{.Name}}_{{.Version}}/libcgo_{{.Name}}_{{.Version}}.so -buildvcs=false -buildmode=c-shared -ldflags="{{.LDFlags}}" {{.House}}/{{.Name}}_{{.Version}}/libcgo_{{.Name}}_{{.Version}} && upx --preserve-build-id -9 {{.House}}/{{.Name}}_{{.Version}}/libcgo_{{.Name}}_{{.Version}}.so
+go build -o {{.House}}/{{.Name}}_{{.Version}}/libgo_{{.Name}}_{{.Version}}.so -buildvcs=false -buildmode=plugin -ldflags="{{.LDFlags}}-r {{.House}}/{{.Name}}_{{.Version}}/" {{.House}}/{{.Name}}_{{.Version}}/libgo_{{.Name}}_{{.Version}} #&& upx --preserve-build-id -9 {{.House}}/{{.Name}}_{{.Version}}/libgo_{{.Name}}_{{.Version}}.so
 `
