dd8ae85d86a222cb561f58c75b757f385faf9462
lizongti <lzongti@gmail.com>
2023-01-05 21:55:55 +0800

    add `go clean --modcache`

--- builder/template_builder_sh.go
+++ builder/template_builder_sh.go
@@ -9,6 +9,7 @@ cd {{.House}}/{{.Name}}_{{.Version}}
 export CGO_ENABLED=1
 export GO111MODULE=on
 export GOPRIVATE={{.Module}}
+go clean --modcache
 go mod tidy
 go build -o {{.House}}/{{.Name}}_{{.Version}}/libcgo_{{.Name}}_{{.Version}}.so -buildvcs=false -buildmode=c-shared -ldflags="{{.LDFlags}}" {{.House}}/{{.Name}}_{{.Version}}/libcgo_{{.Name}}_{{.Version}}
 go build -o {{.House}}/{{.Name}}_{{.Version}}/libgo_{{.Name}}_{{.Version}}.so -buildvcs=false -buildmode=plugin -ldflags="{{.LDFlags}}-r {{.House}}/{{.Name}}_{{.Version}}/" {{.House}}/{{.Name}}_{{.Version}}/libgo_{{.Name}}_{{.Version}}
