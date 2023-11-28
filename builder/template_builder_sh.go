9bdb94136283b7833a8c53b94d275450d189f04f
lizongti <lzongti@gmail.com>
2023-11-28 20:25:59 +0800

    fix template

--- builder/template_builder_sh.go
+++ builder/template_builder_sh.go
@@ -13,6 +13,6 @@ go clean --modcache
 go mod tidy
 go build -o {{.House}}/{{.Name}}/libcgo_{{.Name}}.so -buildvcs=false -buildmode=c-shared -ldflags="{{.LDFlags}}" {{.House}}/{{.Name}}/libcgo_{{.Name}}
 go build -o {{.House}}/{{.Name}}/libgo_{{.Name}}.so -buildvcs=false -buildmode=plugin -ldflags="{{.LDFlags}}-r {{.House}}/{{.Name}}/" {{.House}}/{{.Name}}/libgo_{{.Name}}
-cp -rf {{.House}}/{{.Name}}/libgo_{{.Name}}.so {{.House}}/{{.Name}}/libgo_{{.Name}}.so.$(date "+%Y%m%d%H%M%S")-{{.Version}}
-cp -rf {{.House}}/{{.Name}}/libcgo_{{.Name}}.so {{.House}}/{{.Name}}/libcgo_{{.Name}}.so.$(date "+%Y%m%d%H%M%S")-{{.Version}}
+cp -rf {{.House}}/{{.Name}}/libgo_{{.Name}}.so {{.House}}/{{.Name}}/libgo_{{.Name}}.so.$(date "+%Y%m%d%H%M%S")
+cp -rf {{.House}}/{{.Name}}/libcgo_{{.Name}}.so {{.House}}/{{.Name}}/libcgo_{{.Name}}.so.$(date "+%Y%m%d%H%M%S")
 `
